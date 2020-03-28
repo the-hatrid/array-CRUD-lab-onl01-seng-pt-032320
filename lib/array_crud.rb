ba3c0dbf041e2899d8d1d689719cecc19beff9a8
bruce willis <fashun_vyctum@yahoo.com>
2020-03-28 00:32:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  wow = create_an_array.shift
 end
 
 def remove_element_from_start_of_array(array)
