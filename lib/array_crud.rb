9db4284804fbda43d5c16dd512ccb75df23890bf
bruce willis <fashun_vyctum@yahoo.com>
2020-03-28 00:21:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
   create_an_array.unshift("wow")
   create_an_array[3]
   create_an_array.first
-  create_an_array.last
+  create_an_array.[-1]
   wow = create_an_array.shift
   arrays = create_an_array.pop
 end
