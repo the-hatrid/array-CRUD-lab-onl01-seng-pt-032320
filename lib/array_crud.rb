32e6bb65b8d94ba290053d15d47e5b60c3df6456
bruce willis <fashun_vyctum@yahoo.com>
2020-03-28 00:32:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  wow = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
