621bf0c58f19b6873f9a5864c87c559ea73597c1
bruce willis <fashun_vyctum@yahoo.com>
2020-03-28 00:05:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,8 +4,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["wow", "I","am", "really"
-  ]
+  create_an_array = ["wow", "I","am", "really"]
+  
 end
 
 def add_element_to_end_of_array(array, element)
