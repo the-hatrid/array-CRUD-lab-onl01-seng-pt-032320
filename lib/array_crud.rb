a4d6ccb2cfe770fc21d65f0f6407df8dadc25943
bruce willis <fashun_vyctum@yahoo.com>
2020-03-28 00:49:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,9 +29,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  create_an_array.last
+  array.last
 end
